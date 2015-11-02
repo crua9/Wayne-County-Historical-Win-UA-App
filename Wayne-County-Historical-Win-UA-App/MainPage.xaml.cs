b32d465e4b51603ee768cf28641574e23c1eee37
crua9 <ctbennettii@gmail.com>
2015-11-01 19:49:43 -0500

    Fix
    For whatever reason, the workstation forgot where everything is (buttons
    and everything). I fixed it.
    
    I think a setting on one of my computers caused this.

--- Wayne-County-Historical-Win-UA-App/MainPage.xaml.cs
+++ Wayne-County-Historical-Win-UA-App/MainPage.xaml.cs
@@ -13,22 +13,28 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace Wayne_County_Historical_App
+namespace Wayne_County_Historical_Win_UA_App
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        //Start
         public MainPage()
         {
+            this.InitializeComponent();
             MyFrame.Navigate(typeof(Home));
             //   TitleTextBlock.Text = "Home";
             BackButton.Visibility = Visibility.Collapsed;
             Home.IsSelected = true;
+
+
         }
+
+
 
         //Hamburger Button
         private void HamburgerButton_Click(object sender, RoutedEventArgs e)
@@ -47,11 +53,11 @@ namespace Wayne_County_Historical_App
                 BackButton.Visibility = Visibility.Collapsed;
             }
             /*  else if (Share.IsSelected)
-              {
-                  MyFrame.Navigate(typeof(Share));
-                  //  TitleTextBlock.Text = "Share and Support";
-                  BackButton.Visibility = Visibility.Visible;
-              } */
+               {
+                   MyFrame.Navigate(typeof(Share));
+                   //  TitleTextBlock.Text = "Share and Support";
+                   BackButton.Visibility = Visibility.Visible;
+               } */
             /*  else if (Maps.IsSelected)
               {
                   MyFrame.Navigate(typeof(Maps));
@@ -78,7 +84,6 @@ namespace Wayne_County_Historical_App
              // TitleTextBlock.Text = "Cemtery";
              BackButton.Visibility = Visibility.Visible;
          }
-
           */
         }
 
@@ -92,6 +97,7 @@ namespace Wayne_County_Historical_App
             }
 
         }
+
 
     }
 }
