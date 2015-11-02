84d95d71f5f556cb76727aa11e30768a5f3b3053
crua9 <ctbennettii@gmail.com>
2015-11-02 01:01:41 -0500

    working on the map
    I'm working on the map layout, I need to look at a random area to set
    the test zone.
    I was able to figure out how to click a button and get it to draw, and
    so on.
    
    There needs to be a button which centers the map back to the starting
    point.

--- Wayne-County-Historical-Win-UA-App/CemeteriesLayout.xaml.cs
+++ Wayne-County-Historical-Win-UA-App/CemeteriesLayout.xaml.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Maps;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
@@ -40,5 +43,49 @@ namespace Wayne_County_Historical_Win_UA_App
             base.OnNavigatedTo(e);
         }
 
+        private void Military(object sender, RoutedEventArgs e)
+        {
+            var strokeColor = Colors.DarkBlue;
+            var fillColor = Colors.Blue;
+            foreach (var dataObject in PointList.GetAreas())
+            {
+                var shape = new MapPolygon
+                {
+                    StrokeThickness = 3,
+                    StrokeColor = strokeColor,
+                    FillColor = fillColor,
+                    StrokeDashed = false,
+                    ZIndex = 3,
+                    Path = new Geopath(dataObject.Points.Select(p => p.Position))
+                };
+                shape.AddData(dataObject);
+                Map.MapElements.Add(shape);
+            }
+        }
+
+        private void Emergency(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Something(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ZoomOut(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ZoomIn(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
